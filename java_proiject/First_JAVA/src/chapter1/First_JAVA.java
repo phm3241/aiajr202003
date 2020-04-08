971ccf09a41bcce14ca46c9888af30adb774ab0e
phm3241 <parkhm3241@gmail.com>
2020-04-08 18:26:05 +0900

    Update First_JAVA.java

--- java_proiject/First_JAVA/src/chapter1/First_JAVA.java
+++ java_proiject/First_JAVA/src/chapter1/First_JAVA.java
@@ -4,7 +4,8 @@ public class First_JAVA {
 
 	public static void main(String[] args) {
 		
-		System.out.println("Hello JAVA"); 
+		System.out.println("Hi"); 
+		// System.out.println("Hello JAVA"); 
 
 	}
 
