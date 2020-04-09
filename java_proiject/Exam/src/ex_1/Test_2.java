029ffc5ec460c6423df0913286d335e32957df03
phm3241 <parkhm3241@gmail.com>
2020-04-09 10:37:40 +0900

    Create Test_2.java
    테스트_2_다양하게 출력

---
+++ java_proiject/Exam/src/ex_1/Test_2.java
@@ -0,0 +1,21 @@
+package ex_1;
+
+public class Test_2 {
+
+	public static void main(String[] args) {
+		/* 
+		문제 2.
+		숫자 15를 총 5회 출력하는 프로그램을 작성해 보자.
+		단 총 5회에 걸쳐서 출력이 이루어 져야 하고, 
+		이를 위해서 구성이 되는 다섯 문장 모두 차이가 있어야 한다.
+		*/
+
+		System.out.println(15);       //숫자 15
+		System.out.println("15");     //문자 15
+		System.out.println(10+5);     //연산에 대한 결과     
+		System.out.println(1+"5");    //"1" +"5" 문자열로 변경되어 더해진다.  
+		System.out.println("1"+"5");  //문자열들의 합
+		
+	}
+
+}
