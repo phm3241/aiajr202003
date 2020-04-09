8b0f4ae8eba29019f5eee61fdd6cd50bd9909b9c
phm3241 <parkhm3241@gmail.com>
2020-04-09 15:22:07 +0900

    boolean

---
+++ java_proiject/Exam/src/ex_1/Test_6_boolean.java
@@ -0,0 +1,23 @@
+package ex_1;
+
+public class Test_6_boolean {
+
+	public static void main(String[] args) {
+		
+		boolean b1 = true;   //true, false는 상수처럼 쓰는 키워드다. 
+		boolean b2 = false;
+		
+		int num = 100;
+		
+		System.out.println(b1);
+		System.out.println(b2);	
+		System.out.println("-------------- ");	
+		System.out.println(4>3);   //비교연산에 대한 결과가 참,거짓으로 출력된다. 
+		System.out.println(4<3);   
+		System.out.println("-------------- ");		
+		System.out.println(num<3);   //변수를 비교할 때 사용한다.
+		// >,<,==, >=, <=	
+
+	}
+
+}
