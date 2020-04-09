7770fa507273ba850fab2743a6f8a7f415bf8306
phm3241 <parkhm3241@gmail.com>
2020-04-09 17:18:54 +0900

    Create Test_7_type_int_char.java

---
+++ java_proiject/Exam/src/ex_1/Test_7_type_int_char.java
@@ -0,0 +1,15 @@
+package ex_1;
+
+public class Test_7_type_int_char {
+
+	public static void main(String[] args) {
+		// 대문자 'A'의 유니코드 값과 'Z'의 유니코드 값을 출력하시오.
+		// 형변환 사용하기
+		
+		char ch1 = 'A'; //문자타입의 변수 ch1 선언, 초기화, 'A'저장 대입
+		char ch2 = 'Z'; //문자타입의 변수 ch2 선언, 
+		
+		
+	}
+
+}
