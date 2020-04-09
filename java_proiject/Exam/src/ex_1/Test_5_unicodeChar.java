4d715ab38c75fea93b0d569f32e7c6ba7ed6036f
phm3241 <parkhm3241@gmail.com>
2020-04-09 15:21:58 +0900

    unicodeChar

---
+++ java_proiject/Exam/src/ex_1/Test_5_unicodeChar.java
@@ -0,0 +1,14 @@
+package ex_1;
+
+public class Test_5_unicodeChar {
+
+	public static void main(String[] args) {
+		
+		char c1 = 'A'; // 'A' 가 유니코드의 65로 변경되고, 대입연산이 이루어진다. 
+		char c2 = 66;  // 유니코드값인 66을 직접 넣어주면, B가 출력된다. 
+		
+		System.out.println(c1);
+		System.out.println(c2);
+	}
+
+}
