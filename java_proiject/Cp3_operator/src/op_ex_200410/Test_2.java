6baa68844fb1f3fc2b30a37e8a37fd6cae5ac5cb
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-11 19:04:22 +0900

    연산결과

---
+++ java_proiject/Cp3_operator/src/op_ex_200410/Test_2.java
@@ -0,0 +1,18 @@
+package op_ex_200410;
+
+public class Test_2 {
+
+	public static void main(String[] args) {
+		
+		/* 연산챕터_문제2.
+	      수학식 {{(25x5)+(36-4)}-72}/5 의 계산결과를 출력하는 프로그램 작성. */
+
+		int num = (((25*5)+(36-4))-72)/5;
+		
+		System.out.println(num);    // 17출력. 변수에 식을 대입하고, 변수를 출력해주는 방법
+		System.out.println((((25*5)+(36-4))-72)/5);    // 17출력. 바로 계산식을 출력해주는 방법 
+	
+
+	}
+
+}
