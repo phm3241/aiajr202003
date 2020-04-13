235f577948c345539827badad25873c4bb4ae9f0
phm3241 <parkhm3241@gmail.com>
2020-04-13 18:30:05 +0900

    switch문

---
+++ java_proiject/Cp4_FlowControl/src/ex/Switch_Basic.java
@@ -0,0 +1,36 @@
+package ex;
+
+public class Switch_Basic {
+
+	public static void main(String[] args) {
+		
+		// switch문에서 break를 사용하지 않았을 때
+		int menuNum = 6;
+		
+		switch (menuNum) {
+		
+		case 1:
+			System.out.println("1. Simple JAVA");
+			
+		
+		case 2:
+			System.out.println("2. Funny JAVA");
+		
+		case 3:
+			System.out.println("3. Fantastic JAVA");
+			
+		default:
+			System.out.println("d. The Best Programing Language");
+						
+		
+		}
+		
+		System.out.println("Do you liske coffee?");
+		
+		
+		
+		
+
+	}
+
+}
