ee278b25f2471d234f6df40d7983661eb2b8e863
phm3241 <parkhm3241@gmail.com>
2020-04-13 19:54:08 +0900

    Update ex8.java

--- java_proiject/Cp4_FlowControl/src/ex_200413/ex8.java
+++ java_proiject/Cp4_FlowControl/src/ex_200413/ex8.java
@@ -7,20 +7,25 @@ public class ex8 {
 //		그 출력된 숫자들의 합을 구하는 프로그램을 while 문을 이용해서 작성
 
 		
-		int num = 777;
+		int num = 1;
 		int sum = 0;
 		
 		
 		while (0<num && num<=1000) {
 			if (num%2==0 && num%7==0) {
-				System.out.println(num);
+				System.out.print(num + ", ");
+				System.out.println(" ");
 				sum = sum + num;
 			}
+			num += 1;
 		}
-		
-		System.out.println(sum);
-		
-
+			
+		System.out.println("---------------------------------------------------------");
+		System.out.println("1000이하의 자연수 중 2의 배수이면서 7의 배수인 숫자들의 합:" + sum);
 	}
+				
+		
 
 }
+
+
