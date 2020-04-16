fd03aa5610739c86ed32bed5dc6487ed86681252
phm3241 <parkhm3241@gmail.com>
2020-04-16 19:44:28 +0900

    메서드 만들기

---
+++ java_proiject/Cp5_Method/src/ex/Method_ex.java
@@ -0,0 +1,22 @@
+package ex;
+
+public class Method_ex {
+	// 더하기 기능의 메서드를 가지는 클래스.
+	
+	// 정수형 데이터 두개를 전달받아 
+	// 두 수의 합을 연산하고 
+	// 그 결과를 반환하는 메서드
+	
+	long add(long n1, long n2) {
+		
+		long result = n1 + n2;
+		
+		return result;
+		
+	// 이제 이 코드를 메모리에 올려두어야 메소드로 쓸수있다.
+		
+	}
+	
+	
+	
+}
