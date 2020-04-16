47e28f79e85795459d4fa244207ba84c54f43f77
phm3241 <parkhm3241@gmail.com>
2020-04-16 19:43:42 +0900

    객체지향 클래스 예제_생성자호출

---
+++ java_proiject/Cp6_ClassAndInstance/src/ex/Number.java
@@ -0,0 +1,32 @@
+package ex;
+
+public class Number {
+
+	int num;
+	
+	// 생성자 =클래스이름과 같게 만들어야한다. 
+//	Number(){
+//		System.out.println("생성자 호출");
+//		num = 1000;
+//	}
+//	public static void main(String[] args) {
+//		
+//		Number n1=new Number(); //인스턴스 생성
+//		System.out.println(n1.num); //인스턴스 생성
+//	}
+
+	
+	
+	Number(int n){
+		System.out.println("생성자 호출");
+		num = n;		
+	}
+	
+	public static void main(String[] args) {
+		
+		Number n1=new Number(1000000); //인스턴스 생성
+		System.out.println(n1.num); //인스턴스 생성
+	}
+	
+	
+}
