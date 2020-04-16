f4d362cc5dbed0e43bd628b7d27e96baf5109571
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-17 01:21:38 +0900

    소수판별 메서드(작성중)

---
+++ java_proiject/Cp5_Method/src/ex/PrimeNumber.java
@@ -0,0 +1,30 @@
+//	문제4.
+//	전달되는 값이 소수(prime number)인지 아닌지를 판단하여, 소수인경우 true를, 소수가 아닌 경우 false를 반환하는 메서드를 정의하고, 
+//	이를 이용해서 1이상 100 이하의 소수를 전부 출력할 수 있도록 main 메서드를 정의하자.
+
+
+package ex;
+
+public class PrimeNumber {
+
+	// 전달되는 값이 소수(prime number)인지 아닌지를 판단하여, 
+	// 소수인경우 true를, 소수가 아닌 경우 false를 반환하는 메서드
+//	int checkNum(int n) {
+	
+//		boolean checkNum=
+//		if(checkNum)
+//			return true;
+		}
+	
+//		
+//		
+//}
+
+
+//int base = 180;
+//int height = 185;
+//boolean isTall = height > base;
+//
+//if (isTall) {
+//    System.out.println("키가 큽니다.");
+//}
