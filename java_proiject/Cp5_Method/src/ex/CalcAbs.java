fd9f8d7fca09bb035c6eb363063a44d64f532039
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-17 01:19:59 +0900

    절대값 메서드

---
+++ java_proiject/Cp5_Method/src/ex/CalcAbs.java
@@ -0,0 +1,10 @@
+package ex;
+
+public class CalcAbs {
+	// 두수의 절대값을 계산하여 출력하는 메서드
+
+	// 더하기 메서드 
+	public void abs(long n1, long n2) {
+		System.out.println(n1+"의 절대값: "+Math.abs(n1)+", "+n2+"의 절대값: "+Math.abs(n2));
+	}
+}
