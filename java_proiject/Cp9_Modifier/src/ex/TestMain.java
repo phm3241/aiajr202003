a837c41bafe0df2cbde83b9994e5a745fb9fc59e
phm3241 <parkhm3241@gmail.com>
2020-04-21 19:57:54 +0900

    Create TestMain.java

---
+++ java_proiject/Cp9_Modifier/src/ex/TestMain.java
@@ -0,0 +1,20 @@
+package ex;
+
+public class TestMain {
+
+	public static void main(String[] args) {
+		
+		Member member=new Member();
+		
+//		System.out.println(member.id);
+//		System.out.println(member.name);
+		
+		System.out.println(member.getName());
+		System.out.println(member.getId());
+		
+		Singleton s= Singleton.getInstance();
+		s.print();
+
+	}
+
+}
