97b66c9a337c0b4507cbcc505fc1ac4c1632b4bb
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-23 01:13:37 +0900

    Create PhoneBookMainMenu.java

---
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Try2/PhoneBookMainMenu.java
@@ -0,0 +1,16 @@
+package PhoneBook_Try2;
+
+public class PhoneBookMainMenu {
+	
+	public static void showMenu() {
+		
+		System.out.println("=================");
+		System.out.println("메뉴를 입력해주세요");
+		System.out.println(" 1. 친구 정보 입력");
+		System.out.println(" 2. 친구 정보 검색");
+		System.out.println(" 3. 친구 정보 삭제");
+		System.out.println(" 4. 전체 정보 출력");
+		System.out.println(" 5. 프로그램 종료");
+		System.out.println("=================");
+	}
+}
