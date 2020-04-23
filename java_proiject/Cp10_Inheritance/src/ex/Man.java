ea27200f69be554e547bb3cf3702cc375707fc0c
phm3241 <parkhm3241@gmail.com>
2020-04-23 19:14:50 +0900

    Create Man.java

---
+++ java_proiject/Cp10_Inheritance/src/ex/Man.java
@@ -0,0 +1,19 @@
+package ex;
+
+public class Man {
+	String name;
+	
+	Man(String name){
+		this.name=name;
+	}
+	
+	
+	public void tellYourName() {
+		System.out.println("나의 이름은 "+name+"입니다");
+		
+		
+	}
+
+
+		
+}
