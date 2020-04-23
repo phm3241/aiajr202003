faf57461f29c17531ffeea3a1ec01f3debb24531
phm3241 <parkhm3241@gmail.com>
2020-04-23 19:15:13 +0900

    Create Tv.java

---
+++ java_proiject/Cp10_Inheritance/src/ex/Tv.java
@@ -0,0 +1,24 @@
+package ex;
+
+public class Tv {
+	
+	boolean power;
+	int channel;
+	
+	void power() {
+		power=!power;
+	}
+
+	void channelUp() {
+		channel++;
+	}
+	
+	void channelDown() {
+		channel--;
+	}
+	
+	void display() {
+		System.out.println("Tv 재생합니다.");
+	}
+	
+}
