390fbe489990b723d3fa2600bf9501cb3e1d1f62
phm3241 <parkhm3241@gmail.com>
2020-04-24 14:27:47 +0900

    Create Computer.java

---
+++ java_proiject/Cp10_Inheritance/src/scm/Computer.java
@@ -0,0 +1,17 @@
+package scm;
+
+public class Computer extends Product {
+	
+	Computer(int price){
+		super(price);
+	}
+
+	@Override
+	public String toString() {
+		return "Computer";
+	}
+	void programming() {
+		System.out.println("프로그램을 만듭니다.");
+	}
+
+}
