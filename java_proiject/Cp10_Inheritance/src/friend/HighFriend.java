50e8f1a670b90e3b81a9c022be2096b5716b7003
phm3241 <parkhm3241@gmail.com>
2020-04-24 19:04:44 +0900

    Create HighFriend.java

---
+++ java_proiject/Cp10_Inheritance/src/friend/HighFriend.java
@@ -0,0 +1,21 @@
+package friend;
+
+public class HighFriend extends Friend{
+
+	String work;    // 친구의 직업
+
+	public HighFriend(String name, String phoneNum, String addr, String work) {
+		super(name, phoneNum, addr);
+		this.work=work;
+	}
+
+	@Override
+	void showData() {
+		super.showBasicInfo();
+		System.out.println("직업 : "+this.work);
+	}
+	
+	
+	
+
+}
