c870d00f71906da474ae6722a33d0dfa1d7efe15
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-26 23:59:53 +0900

    Update PhoneCafeInfor.java

--- java_proiject/PhoneBook_ver/src/PhonBook_Ver04/PhoneCafeInfor.java
+++ java_proiject/PhoneBook_ver/src/PhonBook_Ver04/PhoneCafeInfor.java
@@ -1,10 +1,22 @@
 package PhonBook_Ver04;
 
 public class PhoneCafeInfor extends PhoneInfor {
-
-	public PhoneCafeInfor(String name, String phoneNum, String email) {
+	
+	private String work;
+	private String age;
+	
+	public PhoneCafeInfor(String name, String phoneNum, String email, String address, String work, String age) {
 		super(name, phoneNum, email);
-		// TODO Auto-generated constructor stub
+		this.work=work;
+		this.age=age;
 	}
 
+	@Override
+	void showInfo() {
+		super.showInfo();
+		System.out.println("직업 : "+this.work);
+		System.out.println("나이 : "+this.age);
+	}
+	
+	
 }
