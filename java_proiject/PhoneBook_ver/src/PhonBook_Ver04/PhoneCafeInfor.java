5edaa4b2e15992cd7caa7fc73d9d46a0b025eb52
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-26 23:46:14 +0900

    Create PhoneCafeInfor.java

---
+++ java_proiject/PhoneBook_ver/src/PhonBook_Ver04/PhoneCafeInfor.java
@@ -0,0 +1,10 @@
+package PhonBook_Ver04;
+
+public class PhoneCafeInfor extends PhoneInfor {
+
+	public PhoneCafeInfor(String name, String phoneNum, String email) {
+		super(name, phoneNum, email);
+		// TODO Auto-generated constructor stub
+	}
+
+}
