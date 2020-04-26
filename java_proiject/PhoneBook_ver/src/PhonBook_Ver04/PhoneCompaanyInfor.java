ec0df797bfba6c87e097ddcc87064d3f0c79e100
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-04-26 23:59:57 +0900

    Update PhoneCompaanyInfor.java

--- java_proiject/PhoneBook_ver/src/PhonBook_Ver04/PhoneCompaanyInfor.java
+++ java_proiject/PhoneBook_ver/src/PhonBook_Ver04/PhoneCompaanyInfor.java
@@ -2,10 +2,18 @@ package PhonBook_Ver04;
 
 public class PhoneCompaanyInfor extends PhoneInfor {
 	
-	String company;
+	private String company;
 	
 	public PhoneCompaanyInfor(String name, String phoneNum, String email, String company) {
 		super(name, phoneNum, email);
 		this.company=company;
 	}
+
+	@Override
+	void showInfo() {
+		super.showInfo();
+		System.out.println("회사 : "+this.company);
+	}
+	
+	
 }
