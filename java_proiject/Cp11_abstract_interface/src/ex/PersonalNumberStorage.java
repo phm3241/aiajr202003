36bb70b817fddcb283f2ddb84549f23b079adcf7
phm3241 <parkhm3241@gmail.com>
2020-04-27 18:26:11 +0900

    Create PersonalNumberStorage.java

---
+++ java_proiject/Cp11_abstract_interface/src/ex/PersonalNumberStorage.java
@@ -0,0 +1,10 @@
+package ex;
+
+public abstract class PersonalNumberStorage {
+	
+	public abstract void addPersonalInfo(String perNum, String name);
+	public abstract String searchName(String perNum);
+	
+	
+	
+}
