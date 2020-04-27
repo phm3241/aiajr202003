31390b595b467ec9418d88ee09e734ba522fc6a4
phm3241 <parkhm3241@gmail.com>
2020-04-27 18:26:38 +0900

    Create PhoneUnivInfor.java

---
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Ver04_/PhoneUnivInfor.java
@@ -0,0 +1,21 @@
+package PhoneBook_Ver04_;
+
+public class PhoneUnivInfor extends PhoneInfor {
+	
+	String major;   // 전공
+	String grade;   // 학년
+	
+	public PhoneUnivInfor(String name, String phoneNumber, String address, String email, String major, String grade) {
+		super(name, phoneNumber, address, email);
+		this.major=major;
+		this.grade=grade;
+	}
+
+	@Override
+	void showAllInfo() {
+		super.showAllInfo();
+		System.out.println("전공 :" +this.major);
+		System.out.println("학년 :" +this.grade);
+	}
+	
+}
