228a8790fbcc3c0167e916fd209a90d32a5e7c69
phm3241 <parkhm3241@gmail.com>
2020-04-27 18:25:53 +0900

    Create Fighter.java

---
+++ java_proiject/Cp11_abstract_interface/src/ex/Fighter.java
@@ -0,0 +1,16 @@
+package ex;
+
+// Fighter는 반드시 구현해야하는 Fightable 클래스와 가지고 가야하는 Unit클래스. 
+// 두개의 클래스를 상속하는 다중상속의 효과를 볼 수 있다.
+class Fighter extends Unit implements Fightable {
+
+	public void move() {
+	}
+
+	public void attact() {
+	}
+	
+
+	}
+
+
