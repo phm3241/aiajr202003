e2d8d8c7403c84f3f8b49fd9c88e72f84e1ba326
phm3241 <parkhm3241@gmail.com>
2020-04-27 18:26:32 +0900

    Create PhoneCompanyInfor.java

---
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Ver04_/PhoneCompanyInfor.java
@@ -0,0 +1,26 @@
+package PhoneBook_Ver04_;
+
+public class PhoneCompanyInfor extends PhoneInfor {
+	
+	String company;   // 회사이름
+	String dept;   // 부서
+	String job;   // 직급
+	
+	
+	public PhoneCompanyInfor
+	(String name, String phoneNumber, String address, String email, String company, String dept, String job) {
+		super(name, phoneNumber, address, email);
+		this.company=company;
+		this.dept=dept;
+		this.job=job;
+	}
+
+	@Override
+	void showAllInfo() {
+		super.showAllInfo();
+		System.out.println("회사 :" +this.company);
+		System.out.println("부서 :" +this.dept);
+		System.out.println("직급 :" +this.job);
+	}
+	
+}
