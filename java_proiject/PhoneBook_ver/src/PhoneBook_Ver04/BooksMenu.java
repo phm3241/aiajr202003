820f828431757d43c4b41a84728bf16aa0765dad
phm3241 <parkhm3241@gmail.com>
2020-04-27 14:31:15 +0900

    Create BooksMenu.java

---
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Ver04/BooksMenu.java
@@ -0,0 +1,17 @@
+package PhoneBook_Ver04;
+
+public class BooksMenu {
+
+	public static void showMenu() {
+		System.out.println("==================");
+		System.out.println("메뉴를 선택해주세요");
+		System.out.println(" 1.	친구정보 입력");
+		System.out.println(" 2.	친구정보 검색");
+		System.out.println(" 3. 친구정보 삭제");
+		System.out.println(" 4. 친구정보 수정");
+		System.out.println(" 5. 친구정보 전체보기");
+		System.out.println(" 6. 프로그램종료");
+		System.out.println("==================");
+	}
+	
+}
