84ba5e1838f657e04732d7a10a81640536d1ed8d
phm3241 <parkhm3241@gmail.com>
2020-04-28 15:24:58 +0900

    Create Menu.java

---
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Ver05/Menu.java
@@ -0,0 +1,14 @@
+package PhoneBook_Ver05;
+
+public class Menu {
+	
+	public static void showMain() {
+		System.out.println("메뉴를 선택하세요");
+		System.out.println(" 1. 친구정보 입력");
+		System.out.println(" 2. 친구정보 검색");
+		System.out.println(" 3. 친구정보 삭제");
+		System.out.println(" 4. 친구정보 수정");
+		System.out.println(" 5. 친구정보 전체보기");
+		System.out.println(" 6. 프로그램 종료");
+	}
+}
