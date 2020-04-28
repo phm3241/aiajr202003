e4a91e25572cb097040d3648d731d878682fbde5
phm3241 <parkhm3241@gmail.com>
2020-04-28 15:25:11 +0900

    Create Menu.java

---
+++ java_proiject/PhoneBook_ver/src/friend_PhoneBook_ver05/Menu.java
@@ -0,0 +1,18 @@
+package friend_PhoneBook_ver05;
+
+public interface Menu {
+	
+//	public static final이 생략되어있다.
+	
+	int INSERT_HIGH=1;
+	int INSERT_UNIV=2;
+	int PRINT_BASIC=3;
+	int PRINT_ALL=4;
+	int EXIT=5;
+	
+	
+	
+	
+	
+
+}
