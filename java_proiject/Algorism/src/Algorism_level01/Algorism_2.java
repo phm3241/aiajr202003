699d934d4e2e4e4773a2e72caa5a33bffce0cd8a
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-05-03 21:07:19 +0900

    기초알고리즘 문제_코드업데이트

--- java_proiject/Algorism/src/Algorism_level01/Algorism_2.java
+++ java_proiject/Algorism/src/Algorism_level01/Algorism_2.java
@@ -1,19 +1,44 @@
 package Algorism_level01;
 
+import java.util.Scanner;
+
+/*
+작성자 : 박혜미
+작성일시 : 2020.04.12
+수정일시 : 2020.05.03
+수정내용 : 스캐너로 사용자에게 두숫자 입력받는 기능 추가.
+
+----------------------------------------------------------------------
+문제 2. 두 수의 차 구하기 - 절대값을 이용하는 방법을 순차적으로 기술하세요.
+ */
+
 public class Algorism_2 {
 
 	public static void main(String[] args) {
-		// 2. 두 수의 차 구하기 - 절대값을 이용하는 방법
-		// 해결할 수 있는 방법을 순차적으로 기술하세요.
 
-		// 1- 두 숫자를 받는다. 혹은 두 숫자를 저장한다. a, b
-		int a = -1;
-		int b = 3;
+		//1- 변수선언 : 두숫자
+		int num1; 
+		int num2;
+
 		
-		// 2- a*b < 0 이면, a절대값과 b절대값의 합을 출력한다. a - b 결과를 절대값으로 출력한다.
-		int result = a-b == 0 ? 0 : a-b < 0 ? (a-b)*-1:(a-b);
 		
-		System.out.println(a + ", " + b + "의 차이는 " + result + "입니다.");
+		//2- 사용자에게 두 숫자를 입력받는다.
+		Scanner kb=new Scanner(System.in);  // 입력받기 위해 스캐너 임포트
+		
+		System.out.println("두 수의 차를 구하기 위한, 첫번째 숫자를 입력해주세요.");
+		num1=kb.nextInt();
+		kb.nextLine();         // 버퍼발생을 없애기 위해 한번더 입력코드추가
+		
+		System.out.println("두 수의 차를 구하기 위한, 두번째 숫자를 입력해주세요.");
+		num2=kb.nextInt();
+		kb.nextLine();         // 버퍼발생을 없애기 위해 한번더 입력코드추가
+		
+		
+		
+		// 3- 삼항 연산자로 두 수의 차를 절대값으로 출력.
+		int result = num1-num2 == 0 ? 0 : num1-num2 < 0 ? (num1-num2)*-1:(num1-num2);
+		
+		System.out.println(num1 + ", " + num2 + "의 차이는 " + result + "입니다.");
 		
 
 	}
