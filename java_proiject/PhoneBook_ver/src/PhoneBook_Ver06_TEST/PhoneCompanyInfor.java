2afa0ed5cf2f1f6bf45add0cc1a89bea4b930529
phm3241 <parkhm3241@gmail.com>
2020-05-04 16:16:13 +0900

    Create PhoneCompanyInfor.java

---
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Ver06_TEST/PhoneCompanyInfor.java
@@ -0,0 +1,25 @@
+package PhoneBook_Ver06_TEST;
+
+public class PhoneCompanyInfor extends PhoneInfor {
+	
+	String company;   // 회사이름
+	
+	
+	public PhoneCompanyInfor
+	(String name, String phoneNumber, String email, String company) {
+		super(name, phoneNumber, email);
+		this.company=company;
+	}
+
+	
+	// 상속받아서 오버라이드한 메서드에 public 붙이는 이유 :
+	// Interface PhoneInfor_Interface 에서 상속받은 public abstract void showAllInfo(); 이므로,
+	// 상속받은 하위클래스에서 범위가 좁아지면 안되기 때문에, public을 붙여줘야한다. 
+	@Override
+	public void showAllInfo() {
+		showBasicInfo();
+		System.out.println("회사 :" +this.company);
+		System.out.println("--------------------------");
+	}
+	
+}
