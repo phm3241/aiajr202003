a8aeb605200ec95c69875c7e2bd23e87cdb43fd2
phm3241 <parkhm3241@gmail.com>
2020-05-08 13:22:42 +0900

    .

---
+++ java_proiject/Algorism/src/EX_Rental_phm/RentalItem.java
@@ -0,0 +1,15 @@
+package EX_Rental_phm;
+
+public class RentalItem {
+
+	public void showBasicInfo() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public boolean checkTitle(String title) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
