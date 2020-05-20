173ba97a0cfdfd6ece3cf97c1fd5c623ab2c2ded
phm3241 <parkhm3241@gmail.com>
2020-05-20 12:18:03 +0900

    Update 200519_과제2_select 문_마당서점.sql

--- SQL/200519_과제2_select 문_마당서점.sql
+++ SQL/200519_과제2_select 문_마당서점.sql
@@ -1,21 +1,24 @@
 
 --2020.05.19. [과제] sql select 추가 문제
-
-select * from book;
-select * from Customer;
-select * from Orders;
+--◆◆◆
+desc book;
+desc Customer;
+desc Orders;
+--select * from book;
+--select * from Customer;
+--select * from Orders;
 
 
 --1 마당서점의 고객이 요구하는 다음 질문에 대해 SQL 문을 작성하시오.
 
 --(1) 도서번호가 1인 도서의 이름
-select bookid, bookname
+select bookname
 from book
 where bookid=1;
 
 
 --(2) 가격이 20,000원 이상인 도서의 이름
-select bookname, price
+select bookname
 from book
 where price>20000;
 
@@ -53,13 +56,13 @@ from customer;
 
 
 --(4) 2014년 7월4일~7월7일 사이에 주문받은 도서의 주문번호
-select orderid, orderdate
+select orderid --orderdate
 from orders
 where orderdate between '2014-07-04' and '2014-07-07';
 
 
 --(5) 2014년 7월4일~7월7일 사이에 주문받은 도서를 제외한 도서의 주문번호
-select orderid, orderdate
+select orderid --orderdate
 from orders
 where orderdate not between '2014-07-04' and '2014-07-07';
 
