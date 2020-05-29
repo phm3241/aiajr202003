e995655db146201faaa5a261d7fa7e57c3e1f937
phm3241 <parkhm3241@gmail.com>
2020-05-29 16:23:12 +0900

    Update 200529_[문제해결 시나리오] _sql 작성 .sql

--- SQL/200529_[문제해결 시나리오] _sql 작성 .sql
+++ SQL/200529_[문제해결 시나리오] _sql 작성 .sql
@@ -6,12 +6,10 @@
 
 --  #2 요구사항에 맞는 오브젝트를 생성하는 sql을 작성하시오.
 --  1. 대리키 IDX, 회원 아이디, 이름, 비밀번호, 사진이름, 가입일 을 저장할 수 있는
---     이름이 MEMBER인 테이블을 생성하세요. o
---     회원 아이디에는 기본키 제약설정을 하고o, 이름과 비밀번호에는 not nullo,
---     가입일은 기본 값으로 sysdate가 입력되도록 하시오. o
-
---  2. MEMBER 테이블에서 이름 컬럼으로 인덱스 객체를 생성하시오.o
-
+--     이름이 MEMBER인 테이블을 생성하세요. 
+--     회원 아이디에는 기본키 제약설정을 하고, 이름과 비밀번호에는 not null,
+--     가입일은 기본 값으로 sysdate가 입력되도록 하시오. 
+--  2. MEMBER 테이블에서 이름 컬럼으로 인덱스 객체를 생성하시오.
 --  3. MEMBER 테이블에서 회원 아이디, 이름, 사진 정보만을 출력하는 view 객체를 생성하시오.
 --   view의 이름은 member_view 로 생성하시오.
 
@@ -48,3 +46,7 @@ create table MEMBER(
     constraint pu_fr_u_year_ck check (fr_u_year between 1 and 4),
     constraint pu_fr_ref_fk foreign key(fr_ref) REFERENCES phoneInfo_basic(idx) on delete cascade   
 );
+
+
+desc MEMBER;
+
