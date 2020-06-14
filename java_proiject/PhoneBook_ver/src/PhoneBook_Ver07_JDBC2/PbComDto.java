ecd3082a59b3bfb9e39df0a6b811ccebb1284dac
phm3241 <63032588+phm3241@users.noreply.github.com>
2020-06-14 20:47:08 +0900

    Update PbComDto.java

--- java_proiject/PhoneBook_ver/src/PhoneBook_Ver07_JDBC2/PbComDto.java
+++ java_proiject/PhoneBook_ver/src/PhoneBook_Ver07_JDBC2/PbComDto.java
@@ -1,8 +1,14 @@
 package PhoneBook_Ver07_JDBC2;
 
-public class PbComDto extends PbBasicDto {
+public class PbComDto {
 
-	// 변수선언 8개
+	// 변수선언 7개
+	private int idx;
+	private String name;
+	private String phoneNumber;
+	private String email;
+	private String address;
+	private String regdate;
 	private String company;
 
 	// 기본 생성자(필수)
@@ -11,26 +17,21 @@ public class PbComDto extends PbBasicDto {
 	}
 
 	// 매개변수 생성자(개발자 사용)
-	public PbComDto(int idx, String name, String phoneNumber, String email, String address, String regdate, String company) {
+	public PbComDto(int idx, String name, String phoneNumber, String email, String address, String regdate,
+			String company) {
 		super();
+		this.idx = idx;
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.email = email;
+		this.address = address;
+		this.regdate = regdate;
 		this.company = company;
 	}
 
+	
 	// getter/setter
-	public String getCompany() {
-		return company;
-	}
-
-	public void setCompany(String company) {
-		this.company = company;
-	}
-
 	
-	
-	@Override
-	public String toString() {
-		return "PbComDto [company=" + company + "]";
-	}
 
 
 
