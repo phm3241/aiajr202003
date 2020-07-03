95e70c5654baa368dd404110b00eafcd526f13ab
phm3241 <parkhm3241@gmail.com>
2020-07-03 14:28:06 +0900

    Create RequestMemberInfo.java

---
+++ jsp_project/OpenProject/src/model/RequestMemberInfo.java
@@ -0,0 +1,44 @@
+package model;
+
+public class RequestMemberInfo {
+
+	private String id; 
+	private String pw; 
+	private String uname;
+	
+	// 기본생성자는 그대로
+	public RequestMemberInfo() {
+	}
+	
+	
+	// getter, setter
+	public String getId() {
+		return id;
+	}
+	
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getUname() {
+		return uname;
+	}
+	public void setUname(String uname) {
+		this.uname = uname;
+	}
+	
+	// 데이터 확인용
+	@Override
+	public String toString() {
+		return "requestMemberInfo [id=" + id + ", pw=" + pw + ", uname=" + uname + "]";
+	} 
+	
+	
+	
+	
+}
