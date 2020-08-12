ebf605d42d6cf837f7fafd13d0e4a271c18f7333
phm3241 <parkhm3241@gmail.com>
2020-08-12 20:42:10 +0900

    .

---
+++ spring_project/Spring-OpenProject_sample2/src/main/java/com/aia/op/IndexController.java
@@ -0,0 +1,37 @@
+package com.aia.op;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/index")
+	public String index() {
+		
+//		String str = null;
+//		str.toString();
+		
+		return "index";
+	}
+
+	@ExceptionHandler(NullPointerException.class)
+	public String handleNullPointerException(NullPointerException e) {
+		
+		e.printStackTrace();
+		
+		return "error/nullPointer";
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
